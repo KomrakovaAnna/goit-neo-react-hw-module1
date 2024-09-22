6178a7b93a23dd8675e0539365d7ed47c987c2b4
Anna Komrakova <komrakova.anna@gmail.com>
2024-09-23 00:40:30 +0100

    hw1

---
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -0,0 +1,14 @@
+import styles from './FriendListItem.module.css';
+const FriendListItem = ({ name, isOnline, avatar }) => {
+  return (
+    <div className={styles.friendAvatar}>
+      <img src={avatar} alt="Avatar" width="48" />
+      <p>{name}</p>
+      <p className={isOnline ? styles.online : styles.offline}>
+        {isOnline ? 'Online' : 'Offline'}
+      </p>
+    </div>
+  );
+};
+
+export default FriendListItem;
---
+++ src/components/TransactionItem/TransactionItem.jsx
@@ -0,0 +1,11 @@
+export const TransactionItem = ({ type, amount, currency }) => {
+  return (
+    <>
+      <td>{type}</td>
+      <td>{amount}</td>
+      <td>{currency}</td>
+    </>
+  );
+};
+
+export default TransactionItem;
